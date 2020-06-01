d43f230589aeb0125c1dd6a4e75d4b1a73ee94b7
Kevin Yeap <paeynivek@gmail.com>
2020-06-01 14:24:55 -0500

    Add support for Ubuntu setup

---
+++ git_settings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# a better git log
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+# git shortcuts
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+
+# git settings
+git config --global core.editor /usr/local/bin/nvim
+git config --global user.email "paeynivek@gmail.com"
+git config --global user.name "Kevin Yeap"
---
+++ ubuntu/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install neovim
+sudo apt install fzf
+sudo apt install silversearcher-ag
