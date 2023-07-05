91cd01ba02d15caa37c53ecb636a311d4381248c
Kevin Y <paeynivek@gmail.com>
2023-07-05 10:47:20 -0700

    setup ubuntu in WSL/$HOME. Windows folder compatibility bad.
    windows folders don't work well with linux tools. Permissions are messed
    up. Angular can't watch files properly. chmod doesn't work well on
    windows folders. etc.

--- ubuntu/setup.bash
+++ ubuntu/setup.bash
@@ -9,12 +9,11 @@
 # sudo add-apt-repository ppa:neovim-ppa/stable
 # sudo apt-get update
 # sudo apt-get install neovim
-ln -s /mnt/c/Users/paeyn/Documents/workspace/config/ubuntu/vimrc ~/.vimrc
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir -p ~/.config/nvim/
-ln -s ~/.vimrc ~/.config/nvim/init.vim
+ln -s ~/workspace/config/ubuntu/vimrc ~/.vimrc
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# mkdir -p ~/.config/nvim/
+# ln -s ~/.vimrc ~/.config/nvim/init.vim
 
 # setup bash settings
-ln -s /mnt/c/Users/paeyn/Documents/workspace/config/ubuntu/bashrc ~/.bashrc
-
+ln -s ~/workspace/config/ubuntu/bashrc ~/.bashrc
 
